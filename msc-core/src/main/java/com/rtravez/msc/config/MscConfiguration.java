b06356235fb09926edf4bf4e7c18c9fc64773a38
René Trávez <rene.travez@hotmail.com>
2026-09-07 15:10:09 -0500

    migrate

--- msc-core/src/main/java/com/rtravez/msc/config/MscConfiguration.java
+++ msc-core/src/main/java/com/rtravez/msc/config/MscConfiguration.java
@@ -1,7 +1,5 @@
 package com.rtravez.msc.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,12 +12,4 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableJpaRepositories(basePackages = { "com.rtravez.msc.repository" }, repositoryImplementationPostfix = "Impl")
 public class MscConfiguration {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setAmbiguityIgnored(true);
-        return modelMapper;
-    }
-
 }
