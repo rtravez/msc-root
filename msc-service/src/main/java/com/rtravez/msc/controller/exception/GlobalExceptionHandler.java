c1f6e3d3c67fce30777fc465a43d5e39f256ae8f
René Trávez <rene.travez@hotmail.com>
2026-09-11 17:23:14 -0500

    migrate

--- msc-service/src/main/java/com/rtravez/msc/controller/exception/GlobalExceptionHandler.java
+++ msc-service/src/main/java/com/rtravez/msc/controller/exception/GlobalExceptionHandler.java
@@ -87,7 +87,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ProblemDetail> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
         log.warn("Data integrity violation", ex);
         return problemDetail(HttpStatus.CONFLICT,
-                "La cuenta ya existe o los datos violan una restricción de integridad");
+                "Los datos violan una restricción de integridad");
     }
 
     @ExceptionHandler(Exception.class)
